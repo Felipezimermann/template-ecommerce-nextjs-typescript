9f538fc580471e70dbd66ffa3d8c7665b78a7ef1
Felipezimermann <felipe-zimerman-n@hotmail.com>
2022-04-26 20:29:04 -0300

    new setings

---
+++ src/components/header/index.tsx
@@ -0,0 +1,40 @@
+export default function Header(){
+    return(
+        <>
+            <head>
+                <section>
+                    <div>Zimermann</div>
+                    <div><input type='text'/></div>
+                    <div>bem vindo <br/> entre ou se cadastre</div>
+                    <div>carrinho</div>
+                </section>
+                <section>
+                    <nav>
+                        <a>Acessórios de Tecnologia</a>
+                        <a>Ar e Ventilação</a>
+                        <a>Artesanato</a>
+                        <a>Artigos para Festa</a>
+                        <a>Acessórios de Tecnologia</a>
+                        <a>Ar e Ventilação</a>
+                        <a>Artesanato</a>
+                        <a>Artigos para Festa</a>
+                        <a>Áudio</a>
+                        <a>Automotivo</a>
+                        <a>Bebês</a>
+                        <a>Beleza & Perfumaria</a>
+                        <a>Bem-estar Sexual</a>
+                        <a>Brinquedos</a>
+                        <a>Cama, Mesa e Banho</a>
+                        <a>Câmeras e Drones</a>
+                        <a>Casa e Construção</a>
+                        <a>Casa Inteligente</a>
+                        <a>Celular e Smartphone</a>
+                        <a>Colchões</a>
+                        <a>Comércio e Indústria</a>
+                    </nav>
+                </section>
+                
+            </head>
+        </>
+    )
+}

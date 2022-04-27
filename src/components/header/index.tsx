1f3282fec18dd6f16ed5cf2a4dd0319cd6609852
Felipezimermann <felipe-zimerman-n@hotmail.com>
2022-04-26 22:36:21 -0300

    header construction day 1

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,19 @@
 export default function Header(){
     return(
         <>
-            <head>
-                <section>
-                    <div>Zimermann</div>
-                    <div><input type='text'/></div>
-                    <div>bem vindo <br/> entre ou se cadastre</div>
-                    <div>carrinho</div>
+            <header className="header">
+                <section className="section">
+                    <div className="sectionTitle"><h3>Zimermann</h3></div>
+                    <div className="sectionInputImg">
+                        <input className="sectionInput" type='text'/>
+                        <img className="sectionImg" width={30} height={30} src="https://cdn-icons.flaticon.com/png/128/4208/premium/4208531.png?token=exp=1651021896~hmac=ec081bddf7304ad6b3a57d85a9d40bb3"></img>
+                    </div>
+                    <div className="sectionRegister"><b>Bem-vindo</b><br/><a href="#"><u>Entre ou se cadastre</u></a></div>
+                    <div className="sectionCart">
+                        <img width={50} height={50} src="https://cdn-icons-png.flaticon.com/128/7005/7005286.png" className="sectionCartImg"/>
+                        <div className="sectionCartQt">0</div>
+                    </div>
+                    
                 </section>
                 <section>
                     <nav>
@@ -21,7 +28,7 @@ export default function Header(){
                         <a>Áudio</a>
                         <a>Automotivo</a>
                         <a>Bebês</a>
-                        <a>Beleza & Perfumaria</a>
+                        <a>Beleza  Perfumaria</a>
                         <a>Bem-estar Sexual</a>
                         <a>Brinquedos</a>
                         <a>Cama, Mesa e Banho</a>
@@ -31,12 +38,10 @@ export default function Header(){
                         <a>Celular e Smartphone</a>
                         <a>Colchões</a>
                         <a>Comércio e Indústria</a>
-                        <a>Comércio e Indústria</a>
-                        <a>Comércio e Indústria</a>
                     </nav>
                 </section>
                 
-            </head>
+            </header>
         </>
     )
 }
